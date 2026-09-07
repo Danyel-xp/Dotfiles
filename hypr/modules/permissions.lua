3f5a847355e7d4452480fa99ad5173606422c9a8
Danyel-xp <danyellrodrigues023@gmail.com>
2026-09-07 03:39:35 -0300

    feat: Implementar config de permissões

---
+++ hypr/modules/permissions.lua
@@ -0,0 +1,17 @@
+-----------------------
+----- PERMISSÕES -----
+-----------------------
+
+-- Consulte https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
+-- Observe que alterações nas permissões exigem uma reinicialização do Hyprland
+-- e não são aplicadas durante a execução, por motivos de segurança.
+
+-- hl.config({
+--   ecosystem = {
+--     enforce_permissions = true,
+--   },
+-- })
+
+-- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
+-- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
+-- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
