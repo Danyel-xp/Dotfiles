cd2106691699caba4902fe556e546af39266f4eb
Danyel-xp <danyellrodrigues023@gmail.com>
2026-09-07 02:49:09 -0300

    chore: inicializar repositório

---
+++ hypr/hyprland.lua
@@ -0,0 +1,10 @@
+-- /home/Snow/.config/hypr/hyprland.lua
+
+require("modules/binds")
+require("modules/monitors")
+require("modules/env")
+require("modules/appearance")
+require("modules/animations")
+require("modules/input")
+require("modules/rules")
+require("modules/permissions")
