105c894d50451466a5531c85873f702226d8a507
Danyel-xp <danyellrodrigues023@gmail.com>
2026-09-07 03:21:09 -0300

    feat: Implementar variáveis de ambiente

---
+++ hypr/modules/env.lua
@@ -0,0 +1,12 @@
+-- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
+-- /home/Snow/.config/hypr/modules/env.lua
+
+-------------------------------
+---- VARIAVAEIS DE AMBIENTE ---
+-------------------------------
+
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+
