a0bc04e65854ad96afeae0700160d391526070a5
Danyel-xp <danyellrodrigues023@gmail.com>
2026-09-07 03:16:10 -0300

    feat: configurações de inicialização

---
+++ hypr/modules/autostart.lua
@@ -0,0 +1,16 @@
+-- https://wiki.hypr.land/Configuring/Basics/Autostart/
+-- /home/Snow/.config/hypr/modules/autostart.lua
+
+---------------------------------
+--- INICIALIZAÇÃO AUTOMATICA  ---
+---------------------------------
+
+
+-- Execute automaticamente processo necessarios como (como daemons de notificações, barra de status, etc.)
+-- Ou execute seus aplicativos favoritos ao iniciar:
+--
+-- hl.on("hyprland.start", function () 
+--   hl.exec_cmd(terminal)
+--   hl.exec_cmd("nm-applet")
+--   hl.exec_cmd("waybar & hyprpaper")
+-- end)
