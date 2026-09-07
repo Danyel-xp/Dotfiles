9887f8013ffa357d860425d2a681010d362c6029
Danyel-xp <danyellrodrigues023@gmail.com>
2026-09-07 03:29:41 -0300

    feat: Adicionar configurações de monitores

---
+++ hypr/modules/monitors.lua
@@ -0,0 +1,12 @@
+-- /home/Snow/.config/hypr/modules/monitors.lua
+------------------
+---- MONITORS ----
+------------------
+
+
+hl.monitor({
+    output   = "",
+    mode     = "1920x1080@144",
+    position = "auto",
+    scale    = "1",
+})
